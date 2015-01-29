2f7c27be206645377a39bed54b0faade6d4be799
Matt May <matthewbentonmay@gmail.com>
2015-01-28 21:32:08 -0600

    added data files and tests

--- lib/json_processor.rb
+++ lib/json_processor.rb
@@ -1,3 +1,30 @@
-module JsonProcessor
+require 'json'
 
+module JsonProcessor
+  class Processor
+    def initialize(file)
+      file = File.read(file)
+      @parsed = JSON.parse(file)
+    end
+
+    def merged
+      self.merge.to_json
+    end
+
+    def merge
+      d = {}
+      @parsed.each do |h|
+        h.each do |k,v|
+          d[k] ||= {}
+          v.each do |x,y|
+            d[k][x] ||= {}
+            y.each do |a,b|
+              d[k][x][a] ||= b
+            end
+          end
+        end
+      end
+      d
+    end
+  end
 end
--- test/test_json_processor.rb
+++ test/test_json_processor.rb
@@ -1,12 +1,44 @@
 class TestJsonProcessor < Minitest::Test
-  context '' do
-    setup do
-      @file = File.read(File.join('.', 'test', 'data', 'data.json'))
-      @parsed = JSON.parse(@file)
+  context 'processor' do
+    should 'be an instance of the processor class' do
+      processor = JsonProcessor::Processor.new(sample_file('data.json'))
+      assert_kind_of JsonProcessor::Processor, processor
     end
 
-    should 'be an array' do
-      assert_kind_of Array, @parsed
+    should 'merge permissions properly in example datafile' do
+      assert_permissions_merged 'data.json', 'data_result.json'
     end
+
+    should 'merge permissions properly in 2nd example datafile' do
+      assert_permissions_merged 'data2.json', 'data_result_2.json'
+    end
+
+    should 'merge permissions properly in 3rd example datafile' do
+      assert_permissions_merged 'data3.json', 'data_result_3.json'
+    end
+
+    should 'merge permissions properly in 4th example datafile' do
+      assert_permissions_merged 'data4.json', 'data_result_4.json'
+    end
+
+    should 'merge permissions properly in 5th example datafile' do
+      assert_permissions_merged 'data5.json', 'data_result_5.json'
+    end
+
+    should 'merge permissions properly in 6th example datafile' do
+      assert_permissions_merged 'data6.json', 'data_result_6.json'
+    end
+  end
+
+  def sample_file(file)
+    File.join('.', 'test', 'data', file)
+  end
+
+  def assert_permissions_merged(data_file, expected_file)
+    processor = JsonProcessor::Processor.new(sample_file(data_file))
+    merged = processor.merge
+    expected = JSON.parse(File.read(sample_file(expected_file)))
+
+    assert_equal expected, merged
   end
 end
