0b663c1b4dee1ed48d1ba6c6e7106bed363a361f
Matt May <matthewbentonmay@gmail.com>
2015-01-28 22:10:08 -0600

    added comments and thor interface

--- lib/json_processor.rb
+++ lib/json_processor.rb
@@ -4,6 +4,7 @@ module JsonProcessor
   class Processor
     def initialize(file)
       file = File.read(file)
+      # Parse the JSON from the file
       @parsed = JSON.parse(file)
     end
 
@@ -14,11 +15,17 @@ module JsonProcessor
     def merge
       d = {}
       @parsed.each do |h|
+        # Iterate through each array element
         h.each do |k,v|
+          # For each hash, create a key in the `d` hash
+          # equivalent to each `k` and point it to an
+          # empty hash unless it already has a value
           d[k] ||= {}
           v.each do |x,y|
+            # For each sub-hash, repeat the same procedure
             d[k][x] ||= {}
             y.each do |a,b|
+              # Set the access value
               d[k][x][a] ||= b
             end
           end
